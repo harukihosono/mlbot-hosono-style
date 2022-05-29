f47e6dda0e5be58aef53412c0c289240b2ffd827
harukihosono <lukeharu@gmail.com>
2022-05-29 12:11:55 +0900

    aaa

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ ENV PYTHONUNBUFFERED True
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
-RUN pip install responder
+RUN pip install fastapi
+RUN pip install uvicorn[standard]
 RUN pip install google-cloud-firestore
 RUN pip install websocket
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
+CMD ["uvicorn", "main:app", "--reload", "--host", "0.0.0.0", "--port", "8000"]
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import asyncio
-
 #---------------------------------------------------
 # GMOコイン価格取得
 #---------------------------------------------------
@@ -45,33 +43,86 @@ def getDB():
 # #---------------------------------------------------
 # # Webアプリ化
 # #---------------------------------------------------
-import responder
+from typing import List
 
-api = responder.API()
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
-@api.route("/")
-def hello_world(req, resp):
-    resp.text = "hello, world!"
+app = FastAPI()
 
-if __name__ == '__main__':
-    api.run()
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-# from flask import Flask
 
-# app = Flask(__name__)
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
 
 
+manager = ConnectionManager()
 
-# @app.route("/")
-# async def hello_world():
-#     try:
-#         loop = asyncio.get_event_loop()
-#         loop.run_until_complete(asyncio.ensure_future(gmo_get_price()))
-#         loop.close()
-#         data = getDB() #データベース読み込み
-#     except Exception as e:
-#         print(e)
-#     return data
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port=8080)
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
