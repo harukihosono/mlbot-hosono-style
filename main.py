c16729aeddb8ddb0055b35861a64050908025615
harukihosono <lukeharu@gmail.com>
2022-05-29 11:23:34 +0900

    ssss

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ import responder
 
 api = responder.API()
 
-@api.route("/hello/{who}")
-def hello_to(req, resp, *, who):
-    resp.text = f"hello, {who}!"
+@api.route("/")
+def hello_world(req, resp):
+    resp.text = "hello, world!"
 
 if __name__ == '__main__':
     api.run()
