4950513e5e36c7146f4073e78651cf988b8e0f69
harukihosono <lukeharu@gmail.com>
2022-05-29 14:22:39 +0900

    aaaaaa

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
-FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
+FROM python:3.9-slim-buster
 ENV PYTHONUNBUFFERED True
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
-RUN pip install fastapi
-RUN pip install uvicorn[standard]
+RUN pip install Flask gunicorn
 RUN pip install google-cloud-firestore
-RUN pip install websocket-client
-CMD ["uvicorn", "main:app", "--reload", "--host", "0.0.0.0", "--port", "8000"]
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,129 +1,29 @@
-#---------------------------------------------------
-# GMOコイン価格取得
-#---------------------------------------------------
-import json
-import websocket
-async def gmo_get_price():
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp('wss://api.coin.z.com/ws/public/v1')
-
-    def on_open(self):
-        message = {
-            "command": "subscribe",
-            "channel": "ticker",
-            "symbol": "BTC"
-        }
-        ws.send(json.dumps(message))
-
-    def on_message(self, message):
-        setDB(message)
-
-    ws.on_open = on_open
-    ws.on_message = on_message
-
-    ws.run_forever()
-
-#---------------------------------------------------
-# データベース書き込み読み込み
-#---------------------------------------------------
 from google.cloud import firestore
+#---------------------------------------------------
+# DB
+#---------------------------------------------------
 
-
-def setDB(message):
+def setDB():
     db = firestore.Client()
-
-    doc_ref = db.collection("price").document()
-    doc_ref.set(message)
+    doc_ref = db.collection("test").document()
+    doc_ref.set({
+    'created': firestore.SERVER_TIMESTAMP,
+    'name': 'Test'
+    })
 def getDB():
     db = firestore.Client()
-    docs = db.collection("price").get() #データベース読み込み
-    data = docs[0].to_dict() #1番上のデータを辞書型に変換
+    docs = db.collection("test").get() #データベース読み込み
+    data = docs.to_dict() #データを辞書型に変換
     return data
 
-# #---------------------------------------------------
-# # Webアプリ化
-# #---------------------------------------------------
-from typing import List
+from flask import Flask
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route("/")
+def hello_world():
+    data = getDB() #データベース読み込み
+    return data
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
-
-
-manager = ConnectionManager()
-
-
-@app.get("/")
-async def get():
-    await gmo_get_price()
-    return HTMLResponse(html)
-
-
-@app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
